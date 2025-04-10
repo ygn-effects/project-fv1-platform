c9dc8d6cddcc11666bea0e1cd5307ef28240f0c0
yann-ygn <yann@ygn-effects.com>
2025-04-10 18:04:41 +0200

    Added default values and a default constructor

--- firmware/fv1-platform-firmware/src/logic/program.h
+++ firmware/fv1-platform-firmware/src/logic/program.h
@@ -23,16 +23,18 @@ namespace ProgramConstants {
  * @brief Stores a program properties.
  */
 struct Program {
-  const StaticString m_name;          // Program name
-  const bool m_isDelayEffect;         // Is the program a delay effect
-  const bool m_supportsTap;           // Does the program supports tap temp
-  const bool m_supportsExpr;          // Does the program supports the expression pedal input
-  const bool m_isPot0Enabled;         // Is FV-1's POT0 used
-  const bool m_isPot1Enabled;         // Is FV-1's POT1 used
-  const bool m_isPot2Enabled;         // Is FV-1's POT2 used
-  const bool m_isMixPotEnabled;       // Is the MIX pot used
-  const uint16_t m_minDelayInterval;  // Minimum interval if delay effect
-  const uint16_t m_maxDelayInterval;  // Maximum interval if delay effect
+  const StaticString m_name{""};             // Program name
+  const bool m_isDelayEffect{false};         // Is the program a delay effect
+  const bool m_supportsTap{false};           // Does the program supports tap temp
+  const bool m_supportsExpr{false};          // Does the program supports the expression pedal input
+  const bool m_isPot0Enabled{false};         // Is FV-1's POT0 used
+  const bool m_isPot1Enabled{false};         // Is FV-1's POT1 used
+  const bool m_isPot2Enabled{false};         // Is FV-1's POT2 used
+  const bool m_isMixPotEnabled{false};       // Is the MIX pot used
+  const uint16_t m_minDelayInterval{0};      // Minimum interval if delay effect
+  const uint16_t m_maxDelayInterval{1000};   // Maximum interval if delay effect
+
+  Program();
 
   Program(const char* t_name, bool t_isDelay, bool t_suppTap, bool t_suppExpr, uint8_t t_pot0En, uint8_t t_pot1En, uint8_t t_pot2En, uint8_t t_mixPotEn, uint16_t t_minDelay, uint16_t t_maxDelay) :
     m_name(t_name),
