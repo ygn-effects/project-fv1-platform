d70122e182b2c68778ef0d3515f192a8bcd327cf
yann-ygn <yann@ygn-effects.com>
2025-04-10 18:05:51 +0200

    Rewrote the class to inherit the ToggleDevice class

--- firmware/fv1-platform-firmware/src/peripherals/led.cpp
+++ firmware/fv1-platform-firmware/src/peripherals/led.cpp
@@ -1,41 +1,6 @@
-#include "led.h"
-#include "utils/logging.h"
+#include "peripherals/led.h"
 
-void Led::setup() {
-  pinMode(m_pin, OUTPUT);
-  digitalWrite(m_pin, m_ledState);
-}
-
-void Led::turnOn() {
-  setState(HIGH);
-
-  LOG_DEBUG("LED pin %d : ON", m_pin);
-}
-
-void Led::turnOff() {
-  setState(LOW);
-
-  LOG_DEBUG("LED pin %d : OFF", m_pin);
-}
-
-void Led::setState(uint8_t t_state) {
-  m_ledState = t_state;
-  digitalWrite(m_pin, m_ledState);
-
-  LOG_DEBUG("LED pin %d : set %d", m_pin, m_ledState);
-}
-
-void Led::toggle() {
-  setState(!m_ledState);
-
-  LOG_DEBUG("LED pin %d : toggle %d", m_pin, m_ledState);
-}
-
-uint8_t Led::getState() const {
-  return m_ledState;
-}
-
-void Led::blink(uint8_t t_interval) {
+void BlinkLed::blink(uint8_t t_interval) {
   uint32_t currentTime = millis();
 
   if ((currentTime - m_lastBlinkTime) >= t_interval) {
@@ -44,8 +9,6 @@ void Led::blink(uint8_t t_interval) {
   }
 }
 
-void PwmLed::setBrightness(uint8_t t_brightness) {
+void PwmLed::setBrightness(uint8_t t_brightness) const {
   analogWrite(m_pin, t_brightness);
-
-  LOG_DEBUG("PWM LED pin %d : brightness %d", m_pin, t_brightness);
 }
