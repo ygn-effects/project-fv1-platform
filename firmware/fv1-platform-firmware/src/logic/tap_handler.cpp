4cdd4d3c0054b16502d0f938542a7b940f68bad7
yann-ygn <yann@ygn-effects.com>
2025-04-10 08:32:04 +0200

    Added basic functionnality to the class

--- firmware/fv1-platform-firmware/src/logic/tap_handler.cpp
+++ firmware/fv1-platform-firmware/src/logic/tap_handler.cpp
@@ -1,1 +1,84 @@
 #include "logic/tap_handler.h"
+
+void TapHandler::calculateInterval() {
+  m_interval = (m_lastTapTime - m_firstTapTime) / m_timesTapped;
+}
+
+void TapHandler::calculateDivInterval() {
+  switch (m_divValue) {
+    case DivValue::kQuarter:
+      m_divInterval = m_interval;
+      break;
+
+    case DivValue::kEight:
+      m_divInterval = m_interval / 2;
+      break;
+
+    case DivValue::kSixteenth:
+      m_divInterval = m_interval / 4;
+      break;
+
+    case DivValue::kEightTriplet:
+      m_divInterval = m_interval / 3;
+      break;
+
+    case DivValue::kDottedEight:
+      m_divInterval = (m_interval * 3) / 4;
+      break;
+
+    default:
+      break;
+  }
+}
+
+void TapHandler::registerTap(uint32_t t_currentTime) {
+  m_isNewIntervalSet = false;
+
+  if (m_timesTapped == 0 || (t_currentTime - m_lastTapTime) > m_tapTimeout) {
+    // First tap or timeout
+    m_firstTapTime = t_currentTime;
+    m_interval = 0;
+    m_divInterval = 0;
+    m_timesTapped = 1;
+  }
+  else {
+    // Subsequent taps
+    m_timesTapped++;
+
+    if (m_timesTapped >= c_minTaps) {
+      calculateInterval();
+
+      if (m_divState == DivState::kEnabled) {
+        calculateDivInterval();
+      }
+
+      m_isNewIntervalSet = true;
+    }
+  }
+
+  m_lastTapTime = t_currentTime;
+}
+
+DivState TapHandler::getDivState() {
+  return m_divState;
+}
+
+uint16_t TapHandler::getInterval() {
+  return m_interval;
+}
+
+uint16_t TapHandler::getDivInterval() {
+  return m_divInterval;
+}
+
+bool TapHandler::getIsNewIntervalSet() {
+  return m_isNewIntervalSet;
+}
+
+void TapHandler::setDivState(DivState t_state) {
+  m_divState = t_state;
+}
+
+void TapHandler::setTapTimeout(uint16_t t_timeout) {
+  m_tapTimeout = t_timeout;
+}
--- firmware/fv1-platform-firmware/src/logic/tap_handler.h
+++ firmware/fv1-platform-firmware/src/logic/tap_handler.h
@@ -1,5 +1,47 @@
 #pragma once
 
-class TapHandler {
+#include <Arduino.h>
 
+enum class DivState : uint8_t {
+  kDisabled,
+  kEnabled
+};
+
+enum class DivValue : uint8_t {
+  kQuarter,
+  kEight,
+  kSixteenth,
+  kDottedEight,
+  kEightTriplet
+};
+
+class TapHandler {
+  private:
+    DivState m_divState{DivState::kDisabled};   // Interval divider state
+    DivValue m_divValue{DivValue::kQuarter};    // Interval divider value
+    uint16_t m_interval{0};                     // Interval
+    uint16_t m_divInterval{0};                  // Interval / divider value
+
+    uint32_t m_firstTapTime{0};                 // First time tapped
+    uint32_t m_lastTapTime{0};                  // Last time tapped
+    uint8_t m_timesTapped{0};                   // Times tapped
+    uint16_t m_tapTimeout{0};                   // Timeout before reset
+    const uint8_t c_minTaps{2};                 // Min # of taps before triggering an interval calculation
+
+    bool m_isNewIntervalSet{false};             // Callback flag to check if a new interval is set
+
+    void calculateInterval();
+    void calculateDivInterval();
+
+  public:
+    void registerTap(uint32_t t_currentTime);
+
+    DivState getDivState();
+    uint16_t getInterval();
+    uint16_t getDivInterval();
+    bool getIsNewIntervalSet();
+
+    void setDivState(DivState t_state);
+    void setNextDivValue();
+    void setTapTimeout(uint16_t t_timeout);
 };
