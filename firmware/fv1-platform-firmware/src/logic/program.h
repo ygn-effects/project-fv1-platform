a167acd10518417b7c48f08617ff4a7256da5bc9
yann-ygn <yann@ygn-effects.com>
2025-04-10 08:31:36 +0200

    Added constness to the program name and added a constructor

--- firmware/fv1-platform-firmware/src/logic/program.h
+++ firmware/fv1-platform-firmware/src/logic/program.h
@@ -23,7 +23,7 @@ namespace ProgramConstants {
  * @brief Stores a program properties.
  */
 struct Program {
-  StaticString m_name;                // Program name
+  const StaticString m_name;          // Program name
   const bool m_isDelayEffect;         // Is the program a delay effect
   const bool m_supportsTap;           // Does the program supports tap temp
   const bool m_supportsExpr;          // Does the program supports the expression pedal input
@@ -33,4 +33,16 @@ struct Program {
   const bool m_isMixPotEnabled;       // Is the MIX pot used
   const uint16_t m_minDelayInterval;  // Minimum interval if delay effect
   const uint16_t m_maxDelayInterval;  // Maximum interval if delay effect
+
+  Program(const char* t_name, bool t_isDelay, bool t_suppTap, bool t_suppExpr, uint8_t t_pot0En, uint8_t t_pot1En, uint8_t t_pot2En, uint8_t t_mixPotEn, uint16_t t_minDelay, uint16_t t_maxDelay) :
+    m_name(t_name),
+    m_isDelayEffect(t_maxDelay),
+    m_supportsTap(t_suppTap),
+    m_supportsExpr(t_suppExpr),
+    m_isPot0Enabled(t_pot0En),
+    m_isPot1Enabled(t_pot1En),
+    m_isPot2Enabled(t_pot2En),
+    m_isMixPotEnabled(t_mixPotEn),
+    m_minDelayInterval(t_minDelay),
+    m_maxDelayInterval(t_maxDelay) {}
 };
