6b74e34a46843376419f0e4b85d63659a41ab213
yann-ygn <yann@ygn-effects.com>
2025-04-11 17:18:09 +0200

    Basic implementation of the DeviceState saving/loading

---
+++ firmware/fv1-platform-firmware/src/logic/memory_manager.cpp
@@ -0,0 +1,103 @@
+#include "logic/memory_manager.h"
+
+void MemoryManager::serializeDeviceState(const DeviceState& t_state, uint8_t* t_buffer) const {
+  // Device state basic data
+  t_buffer[0] = static_cast<uint8_t>(t_state.getBypassState());
+  t_buffer[1] = static_cast<uint8_t>(t_state.getProgramMode());
+  t_buffer[2] = t_state.getCurrentProgram();
+  t_buffer[3] = t_state.getCurrentPreset();
+  t_buffer[4] = t_state.getMidiChannel();
+
+  // Tap handler data
+  t_buffer[5] = static_cast<uint8_t>(t_state.getTapHandler().getDivState());
+  t_buffer[6] = static_cast<uint8_t>(t_state.getTapHandler().getDivValue());
+
+  uint16_t interval = t_state.getTapHandler().getInterval();
+  uint16_t divInterval = t_state.getTapHandler().getDivInterval();
+  uint8_t low = 0, high = 0;
+
+  Utils::pack16(interval, low, high);
+  t_buffer[7] = low;
+  t_buffer[8] = high;
+
+  Utils::pack16(divInterval, low, high);
+  t_buffer[9] = low;
+  t_buffer[10] = high;
+
+  // Expr handlers data
+  for (uint8_t i = 0; i < ProgramConstants::c_maxPrograms; i++) {
+    uint8_t handlerOffset = MemoryManagerConstants::c_stateSize + MemoryManagerConstants::c_tapSize + (i * MemoryManagerConstants::c_exprSize); // Starts at byte 11 and 7 bytes per handler
+
+    t_buffer[handlerOffset] = static_cast<uint8_t>(t_state.getExprHandler(i).getState());
+    t_buffer[handlerOffset + 1] = static_cast<uint8_t>(t_state.getExprHandler(i).getMappedPot());
+    t_buffer[handlerOffset + 2] = static_cast<uint8_t>(t_state.getExprHandler(i).getDirection());
+
+    uint16_t heelValue = t_state.getExprHandler(i).getHeelValue();
+    uint16_t toeValue = t_state.getExprHandler(i).getToeValue();
+
+    Utils::pack16(heelValue, low, high);
+    t_buffer[handlerOffset + 3] = low;
+    t_buffer[handlerOffset + 4] = high;
+
+    Utils::pack16(toeValue, low, high);
+    t_buffer[handlerOffset + 5] = low;
+    t_buffer[handlerOffset + 6] = high;
+  }
+}
+
+void MemoryManager::deserializeDeviceState(DeviceState& t_state, const uint8_t* t_buffer) const {
+  // Device state basic data
+  t_state.setBypassState(static_cast<BypassState>(t_buffer[0]));
+  t_state.setProgramMode(static_cast<ProgramMode>(t_buffer[1]));
+  t_state.setCurrentProgram(t_buffer[2]);
+  t_state.setCurrentPreset(t_buffer[3]);
+  t_state.setMidiChannel(t_buffer[4]);
+
+  // Tap handler data
+  t_state.getTapHandler().setDivState(static_cast<DivState>(t_buffer[5]));
+  t_state.getTapHandler().setDivValue(static_cast<DivValue>(t_buffer[6]));
+
+  uint16_t interval = 0, divInterval = 0;
+  Utils::unpack16(t_buffer[7], t_buffer[8], interval);
+  Utils::unpack16(t_buffer[9], t_buffer[10], divInterval);
+
+  t_state.getTapHandler().setInterval(interval);
+  t_state.getTapHandler().setDivInterval(divInterval);
+
+  // Expr handlers data
+  for (uint8_t i = 0; i < ProgramConstants::c_maxPrograms; i++) {
+    uint8_t handlerOffset = MemoryManagerConstants::c_stateSize + MemoryManagerConstants::c_tapSize + (i * MemoryManagerConstants::c_exprSize); // Starts at byte 11 and 7 bytes per handler
+
+    t_state.getExprHandler(i).setState(static_cast<ExprState>(t_buffer[handlerOffset]));
+    t_state.getExprHandler(i).setMappedPot(static_cast<MappedPot>(t_buffer[handlerOffset + 1]));
+    t_state.getExprHandler(i).setDirection(static_cast<Direction>(t_buffer[handlerOffset + 2]));
+
+    uint16_t heelValue = 0, toeValue = 0;
+    Utils::unpack16(t_buffer[handlerOffset + 3], t_buffer[handlerOffset + 4], heelValue);
+    Utils::unpack16(t_buffer[handlerOffset + 5], t_buffer[handlerOffset + 6], toeValue);
+
+    t_state.getExprHandler(i).setHeelToeValues(heelValue, toeValue);
+  }
+}
+
+void MemoryManager::saveDeviceState(const DeviceState& t_state) {
+  uint16_t address = MemoryManagerConstants::c_deviceStateStartAddress;
+  uint8_t buffer[MemoryManagerConstants::c_deviceStateSize]{0};
+
+  serializeDeviceState(t_state, buffer);
+
+  for (uint16_t i = 0; i < MemoryManagerConstants::c_deviceStateSize; i++) {
+    eeprom.writeInt8(address + i, buffer[i]);
+  }
+}
+
+void MemoryManager::loadDeviceState(DeviceState& t_state) {
+  uint16_t address = MemoryManagerConstants::c_deviceStateStartAddress;
+  uint8_t buffer[MemoryManagerConstants::c_deviceStateSize]{0};
+
+  for (uint16_t i = 0; i < MemoryManagerConstants::c_deviceStateSize; i++) {
+    buffer[i] = eeprom.readInt8(address + i);
+  }
+
+  deserializeDeviceState(t_state, buffer);
+}
--- firmware/fv1-platform-firmware/src/logic/memory_manager.h
+++ firmware/fv1-platform-firmware/src/logic/memory_manager.h
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 #include "peripherals/eeprom.h"
 #include "logic/device_state.h"
+#include "utils/utils.h"
 
 /*
  * Memory Map for Device state Storage in EEPROM
@@ -17,20 +18,40 @@
  * 5-10             tapHandler         Tap handler data
  *                   | divState        Div state
  *                   | divValue        Div value
- *                   | interval        Interval byte 1
- *                   | interval        Interval byte 2
- *                   | divInterval     Div interval byte 1
- *                   | divinterval     Div interval byte 2
+ *                   | interval        Interval low byte
+ *                   | interval        Interval high byte
+ *                   | divInterval     Div interval low byte
+ *                   | divinterval     Div interval high byte
+ * 11-66            exprHandler        Expr handler data x8
+ *                   | state           Expr state
+ *                   | mappedPot       Expr mapped pot
+ *                   | direction       Expr direction
+ *                   | heelValue       Heel value low byte
+ *                   | heelValue       Heel value high byte
+ *                   | toeValue        Toe value low byte
+ *                   | toeValue        Toe value high byte
  */
 
 namespace MemoryManagerConstants {
   constexpr uint8_t c_deviceStateStartAddress = 0x0;
-  constexpr uint8_t c_deviceStateSize = 0xA;
+  constexpr uint8_t c_stateSize = 5;
+  constexpr uint8_t c_tapSize = 6;
+  constexpr uint8_t c_exprSize = 7;
+  constexpr uint8_t c_exprCount = ProgramConstants::c_maxPrograms;
+  constexpr uint8_t c_deviceStateSize = MemoryManagerConstants::c_stateSize + MemoryManagerConstants::c_tapSize + (MemoryManagerConstants::c_exprSize * MemoryManagerConstants::c_exprCount);
 }
 
 class MemoryManager {
   private:
+    Eeprom eeprom;
 
+    void serializeDeviceState(const DeviceState& t_state, uint8_t* t_buffer) const;
+    void deserializeDeviceState(DeviceState& t_state, const uint8_t* t_buffer) const;
 
   public:
+    MemoryManager(uint8_t t_eepromCsPin) :
+      eeprom(t_eepromCsPin) {}
+
+    void saveDeviceState(const DeviceState& t_state);
+    void loadDeviceState(DeviceState& t_state);
 };
