7c790bbc8fa918fa1c1dfef564e6ff825c4c6c27
yann-ygn <yann@ygn-effects.com>
2025-05-05 21:22:12 +0200

    Consolidated the  utils functions into the header

--- firmware/fv1-platform-firmware/src/utils/utils.h
+++ firmware/fv1-platform-firmware/src/utils/utils.h
@@ -3,10 +3,14 @@
 #include <stdint.h>
 
 namespace Utils {
-  const char* numberToString(uint8_t number);
+  constexpr void pack16(uint16_t value, uint8_t& lowByte, uint8_t& highByte) {
+    lowByte = value & 0xFF;
+    highByte = (value >> 8) & 0xFF;
+  }
 
-  void pack16(uint16_t value, uint8_t& lowByte, uint8_t& highByte);
-  void unpack16(const uint8_t lowByte, const uint8_t highByte, uint16_t& value);
+  constexpr void unpack16(const uint8_t lowByte, const uint8_t highByte, uint16_t& value) {
+    value = static_cast<uint16_t>(lowByte) | (static_cast<uint16_t>(highByte) << 8);
+  }
 
   template<typename T>
   static inline T clamp(T val, T min_val, T max_val) {
