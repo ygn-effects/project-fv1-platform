da6c3ba4efaaa3068e4f03248fc92f995a6bc1ae
yann-ygn <yann@ygn-effects.com>
2025-05-05 21:23:07 +0200

    Added a next enum utility functions

--- firmware/fv1-platform-firmware/src/utils/enum_utils.h
+++ firmware/fv1-platform-firmware/src/utils/enum_utils.h
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 
+namespace EnumUtils {
+
 template<typename E, E... ValidValues>
 struct EnumValidator {
   static constexpr bool isValid(uint8_t t_rawValue) {
@@ -12,3 +14,10 @@ struct EnumValidator {
     return isValid(t_rawValue) ? static_cast<E>(t_rawValue) : t_fallback;
   }
 };
+
+template<typename E, uint8_t Count>
+constexpr E nextEnumValue(E value) {
+  return static_cast<E>((static_cast<uint8_t>(value) + 1) % Count);
+}
+
+}
