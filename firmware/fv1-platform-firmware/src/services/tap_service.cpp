6a104a09f628a33d8a216f4d09d264e81873130f
yann-ygn <yann@ygn-effects.com>
2025-05-06 15:37:45 +0200

    Reflected changes made to Taphandler

--- firmware/fv1-platform-firmware/src/services/tap_service.cpp
+++ firmware/fv1-platform-firmware/src/services/tap_service.cpp
@@ -4,19 +4,19 @@ void TapService::publishTapIntervalEvent(const Event& t_event) const {
   Event e;
   e.m_type = EventType::kTapIntervalChanged;
   e.m_timestamp = t_event.m_timestamp;
-  e.m_data.value = m_tapHandler.getDivState() == DivState::kDisabled
-                    ? m_tapHandler.getInterval()
-                    : m_tapHandler.getDivInterval();
+  e.m_data.value = m_tapHandler.m_divState == DivState::kDisabled
+                    ? m_tapHandler.m_interval
+                    : m_tapHandler.m_divInterval;
 
   EventBus::publish(e);
 }
 
 void TapService::init() {
-  m_tapHandler.setTapState(m_logicalState.m_tapState);
-  m_tapHandler.setDivState(m_logicalState.m_divState);
-  m_tapHandler.setDivValue(m_logicalState.m_divValue);
-  m_tapHandler.setInterval(m_logicalState.m_interval);
-  m_tapHandler.setDivInterval(m_logicalState.m_divInterval);
+  m_tapHandler.m_tapState = m_logicalState.m_tapState;
+  m_tapHandler.m_divState = m_logicalState.m_divState;
+  m_tapHandler.m_divValue = m_logicalState.m_divValue;
+  m_tapHandler.m_interval = m_logicalState.m_interval;
+  m_tapHandler.m_divInterval = m_logicalState.m_divInterval;
 }
 
 void TapService::handleEvent(const Event& t_event) {
@@ -36,21 +36,21 @@ void TapService::handleEvent(const Event& t_event) {
   if (t_event.m_type == EventType::kTapPressed || t_event.m_type == EventType::kMidiTapPressed && t_event.m_data.value == MidiCCValues::c_tapShortPress) {
     m_tapHandler.registerTap(t_event.m_timestamp);
 
-    if (m_tapHandler.getIsNewIntervalSet()) {
-      m_logicalState.m_interval = m_tapHandler.getInterval();
+    if (m_tapHandler.m_isNewIntervalSet) {
+      m_logicalState.m_interval = m_tapHandler.m_interval;
 
       publishTapIntervalEvent(t_event);
     }
 
-    m_logicalState.m_tapState = m_tapHandler.getTapState();
+    m_logicalState.m_tapState = m_tapHandler.m_tapState;
   }
   else if (t_event.m_type == EventType::kTapLongPressed && m_logicalState.m_tapState == TapState::kEnabled
         || t_event.m_type == EventType::kMidiTapPressed && t_event.m_data.value == MidiCCValues::c_tapLongPress && m_logicalState.m_tapState == TapState::kEnabled) {
     m_tapHandler.setNextDivValue();
 
-    m_logicalState.m_divState = m_tapHandler.getDivState();
-    m_logicalState.m_divValue = m_tapHandler.getDivValue();
-    m_logicalState.m_divInterval = m_tapHandler.getDivInterval();
+    m_logicalState.m_divState = m_tapHandler.m_divState;
+    m_logicalState.m_divValue = m_tapHandler.m_divValue;
+    m_logicalState.m_divInterval = m_tapHandler.m_divInterval;
 
     publishTapIntervalEvent(t_event);
   }
