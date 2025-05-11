e03d800d2981a3f011356d48abba834342642b37
yann-ygn <yann@ygn-effects.com>
2025-05-11 23:06:58 +0200

    Added the SSD1306 driver and tests

---
+++ firmware/fv1-platform-firmware/src/hal/display_ssd1306.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <stdint.h>
+#include "Adafruit_SSD1306.h"
+#include "Adafruit_GFX.h"
+#include "Wire.h"
+#include "hal/display.h"
+
+class SSD1306 : public DisplayDriver {
+  private:
+    Adafruit_SSD1306 m_ssd1306;
+
+  public:
+    SSD1306() :
+      m_ssd1306(128, 64, &Wire, -1) {}
+
+    void init() override {
+      m_ssd1306.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+      m_ssd1306.setTextSize(1);
+      m_ssd1306.setTextColor(SSD1306_WHITE);
+    }
+
+    void clear() override {
+      m_ssd1306.clearDisplay();
+    }
+
+    void drawRect(int16_t t_x, int16_t t_y, int16_t t_w, int8_t t_h, bool t_filled = true) override {
+      if (t_filled) {
+        m_ssd1306.fillRect(t_x, t_y, t_w, t_h, SSD1306_WHITE);
+      }
+      else {
+        m_ssd1306.drawRect(t_x, t_y, t_w, t_h, SSD1306_WHITE);
+      }
+    }
+
+    void drawText(int16_t t_x, int16_t t_y, const char* t_text, bool t_inverted = false) {
+      m_ssd1306.setCursor(t_x, t_y);
+
+      if (t_inverted) {
+        m_ssd1306.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+        m_ssd1306.print(t_text);
+        m_ssd1306.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
+      }
+      else {
+        m_ssd1306.print(t_text);
+      }
+    }
+
+    void display() override {
+      m_ssd1306.display();
+    }
+
+    int16_t getWidth() const override { return static_cast<int16_t>(128); }
+    int16_t getHeight() const override { return static_cast<int16_t>(64); }
+
+    uint16_t getTextWidth(const char* t_text) {
+      int16_t x1, y1;
+      uint16_t width, height;
+      m_ssd1306.getTextBounds(t_text, 0, 0, &x1, &y1, &width, &height);
+
+      return width;
+    }
+
+    uint16_t getTextHeight(const char* t_text) {
+      int16_t x1, y1;
+      uint16_t textWidth, height;
+      m_ssd1306.getTextBounds(t_text, 0, 0, &x1, &y1, &textWidth, &height);
+
+      return height;
+    }
+
+    uint16_t getLineHeight() { return getTextHeight("A"); }
+};
---
+++ firmware/fv1-platform-firmware/test/embedded/hal/test_display_ssd1306/test_display_ssd1306.cpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+#include <unity.h>
+
+#include "hal/display_ssd1306.h"
+
+void setUp(void) {
+
+}
+
+void tearDown() {
+
+}
+
+void test_text() {
+  SSD1306 m_ssd1306;
+  m_ssd1306.init();
+  m_ssd1306.clear();
+
+  uint16_t lineHeight = m_ssd1306.getLineHeight();
+  m_ssd1306.drawText(0, 0, "Test test test");
+  m_ssd1306.drawText(0, lineHeight + 1, "Test test test", true);
+  m_ssd1306.display();
+
+  TEST_ASSERT_TRUE(true);
+}
+
+void test_rect() {
+  SSD1306 m_ssd1306;
+  m_ssd1306.init();
+  m_ssd1306.clear();
+
+  uint16_t lineHeight = m_ssd1306.getLineHeight();
+  m_ssd1306.drawRect(0, 0, m_ssd1306.getWidth(), lineHeight);
+  m_ssd1306.drawRect(0, lineHeight + 1, m_ssd1306.getWidth(), lineHeight, false);
+  m_ssd1306.display();
+
+  TEST_ASSERT_TRUE(true);
+}
+
+void setup() {
+  delay(2000);
+
+  UNITY_BEGIN();
+  RUN_TEST(test_text);
+  delay(5000);
+  RUN_TEST(test_rect);
+  delay(5000);
+  UNITY_END();
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(500);
+}
