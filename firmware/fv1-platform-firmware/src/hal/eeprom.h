0b29fcd79d8e0da168d936d8a836a3fe75a48014
yann-ygn <yann@ygn-effects.com>
2025-05-18 22:14:25 +0200

    Added an init() method to the EEPROM base class and mocked version

--- firmware/fv1-platform-firmware/src/hal/eeprom.h
+++ firmware/fv1-platform-firmware/src/hal/eeprom.h
@@ -4,6 +4,7 @@
 
 class EEPROM {
   public:
+    virtual void init() = 0;
     virtual void read(uint16_t t_address, uint8_t* t_data, size_t t_length) = 0;
     virtual void write(uint16_t t_address, const uint8_t* t_data, size_t t_length) = 0;
 };
--- firmware/fv1-platform-firmware/src/mock/mock_eeprom.h
+++ firmware/fv1-platform-firmware/src/mock/mock_eeprom.h
@@ -3,20 +3,29 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <algorithm>
+#include <array>
 #include "hal/eeprom.h"
 
 class MockEEPROM : public EEPROM {
   public:
     static constexpr size_t c_size = 32768;
-    uint8_t m_memory[c_size] = {0};
+    inline static std::array<uint8_t, c_size> m_memory;
+
+    void init() override {
+
+    }
 
     void read(uint16_t t_address, uint8_t* t_data, size_t t_length) override {
       if (t_address + t_length > c_size) return;
-      std::copy(m_memory + t_address, m_memory + t_address + t_length, t_data);
+      std::copy(std::begin(m_memory) + t_address, std::begin(m_memory) + t_address + t_length, t_data);
     }
 
     void write(uint16_t t_address, const uint8_t* t_data, size_t t_length) override  {
       if (t_address + t_length > c_size) return;
-      std::copy(t_data, t_data + t_length, m_memory + t_address);
+      std::copy(t_data, t_data + t_length, std::begin(m_memory) + t_address);
+    }
+
+    void reset() {
+      m_memory.fill(0);
     }
 };
