f6d38e6af067022aaea1cbf9ee4164729bc1e9b1
yann-ygn <yann@ygn-effects.com>
2025-06-01 10:31:27 +0200

    Added a mocked adjustable LED

--- firmware/fv1-platform-firmware/src/mock/mock_led.h
+++ firmware/fv1-platform-firmware/src/mock/mock_led.h
@@ -1,24 +1,50 @@
 #pragma once
 
 #include <stdint.h>
+#include "periphs/adjustable.h"
 #include "periphs/toggleable.h"
 
-class Led : public Toggleable {
-  uint8_t m_pinState = 0;
+class MockLed : public Toggleable {
+  public:
+    bool initialized = false;
+    uint8_t m_pinState = 0;
 
-  void init() override {
+    void init() override {
+      initialized = true;
+    }
 
-  }
+    void on() override {
+      if (!initialized) return;
+      m_pinState = 1;
+    }
 
-  void on() override {
-    m_pinState = 1;
-  }
+    void off() override {
+      if (!initialized) return;
+      m_pinState = 0;
+    }
 
-  void off() override {
-    m_pinState = 0;
-  }
+    void toggle() override {
+      if (!initialized) return;
+      m_pinState = !m_pinState;
+    }
+};
 
-  void toggle() override {
-    m_pinState = !m_pinState;
-  }
+class MackAdjustbleLed : public Adjustable {
+  public:
+    bool initialized = false;
+    uint8_t m_pinValue = 0;
+
+    void init() override {
+      initialized = true;
+    }
+
+    void off() override {
+      if (!initialized) return;
+      m_pinValue = 255;
+    }
+
+    void setValue(uint8_t t_value) override {
+      if (!initialized) return;
+      m_pinValue = t_value;
+    }
 };
