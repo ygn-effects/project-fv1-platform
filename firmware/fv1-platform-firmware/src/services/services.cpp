442153fc0acd6a54baced024a314b848e2fe2833
yann-ygn <yann@ygn-effects.com>
2025-06-02 08:52:41 +0200

    Added the remaining services

--- firmware/fv1-platform-firmware/src/services/services.cpp
+++ firmware/fv1-platform-firmware/src/services/services.cpp
@@ -10,6 +10,11 @@ MemoryService memoryService(logicalState, hal::eeprom);
 ProgramModeService programModeService(logicalState);
 ProgramService programService(logicalState);
 BypassService bypassService(logicalState, hal::bypass);
+PotService potService(logicalState);
+TapService tapService(logicalState);
+TempoService tempoService(logicalState, hal::tapSwitchLed, hal::clock);
+MenuService menuService(logicalState, hal::clock);
+DisplayService displayService(logicalState, hal::display);
 
 ServiceManager serviceManager;
 
@@ -20,6 +25,11 @@ void init() {
   serviceManager.registerService(&programModeService);
   serviceManager.registerService(&programService);
   serviceManager.registerService(&bypassService);
+  serviceManager.registerService(&potService);
+  serviceManager.registerService(&tapService);
+  serviceManager.registerService(&tempoService);
+  serviceManager.registerService(&menuService);
+  serviceManager.registerService(&displayService);
 
   serviceManager.init();
 }
