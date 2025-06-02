893902e6bce500ca98c98056f085f707e9d4fee5
yann-ygn <yann@ygn-effects.com>
2025-06-02 08:52:28 +0200

    Added main loop

--- firmware/fv1-platform-firmware/src/main.cpp
+++ firmware/fv1-platform-firmware/src/main.cpp
@@ -1,7 +1,21 @@
-void setup() {
+#include <Arduino.h>
+#include "hal/hal.h"
+#include "services/services.h"
 
+void setup() {
+  Serial.begin(31250);
+  delay(2000);
+
+  Serial.println("Setup...");
+  hal::init();
+  services::init();
+
+  EventBus::publish({EventType::kBootCompleted, 0, {}});
+
+  Serial.println("Setup done, starting main loop...");
 }
 
-int main() {
-
+void loop() {
+  hal::update();
+  services::update();
 }
