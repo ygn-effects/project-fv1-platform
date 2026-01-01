fd4b9a89c33a8d76eb4b46798a81aace990c6e56
yann-ygn <yann@ygn-effects.com>
2026-01-01 21:01:28 +0100

    Refactored the service with the new event system

--- firmware/fv1-platform-firmware/src/services/bypass_service.cpp
+++ firmware/fv1-platform-firmware/src/services/bypass_service.cpp
@@ -1,7 +1,23 @@
 #include "services/bypass_service.h"
 
-void BypassService::publishSaveBypassEvent(const Event& t_event) {
-  EventBus::publish({EventType::kSaveBypass, t_event.m_timestamp /*millis()*/, {}});
+void BypassService::publishSaveBypassEvent(const Event& t_event) const {
+  Event e;
+  e.m_domain = EventDomain::kMemory;
+  e.m_subject = EventSubject::kBypass;
+  e.m_action = EventAction::kSave;
+  e.m_timestamp = t_event.m_timestamp;
+
+  EventBus::publish(e);
+}
+
+void BypassService::publishBypassToggledEvent(const Event& t_event) const {
+  Event e;
+  e.m_domain = EventDomain::kLogic;
+  e.m_subject = EventSubject::kBypass;
+  e.m_action = EventAction::kToggled;
+  e.m_timestamp = t_event.m_timestamp;
+
+  EventBus::publish(e);
 }
 
 void BypassService::init() {
@@ -13,17 +29,16 @@ void BypassService::init() {
 }
 
 void BypassService::handleEvent(const Event& t_event) {
-  switch (t_event.m_type) {
-    case EventType::kBypassPressed:
+  switch (t_event.m_domain) {
+    case EventDomain::kPhysical:
       m_logicalState.m_bypassState = m_logicalState.m_bypassState == BypassState::kActive
         ? BypassState::kBypassed
         : BypassState::kActive;
 
       m_bypass.toggle();
-      publishSaveBypassEvent(t_event);
       break;
 
-    case EventType::kMidiBypassPressed:
+    case EventDomain::kMidi:
       if (t_event.m_data.value == MidiCCValues::c_bypassDisable) {
         m_logicalState.m_bypassState = BypassState::kBypassed;
         m_bypass.off();
@@ -39,16 +54,22 @@ void BypassService::handleEvent(const Event& t_event) {
       break;
   }
 
-  EventBus::publish({m_logicalState.m_bypassState == BypassState::kActive
-    ? EventType::kBypassEnabled
-    : EventType::kBypassDisabled, t_event.m_timestamp, {}});
+  publishBypassToggledEvent(t_event);
+  publishSaveBypassEvent(t_event);
 }
 
 void BypassService::update() {
 
 }
 
-bool BypassService::interestedIn(EventCategory t_category, EventSubCategory t_subCategory) const {
-  return (t_category == EventCategory::kPhysicalEvent && t_subCategory == EventSubCategory::kBypassEvent)
-      || (t_category == EventCategory::kMidiEvent && t_subCategory == EventSubCategory::kMidiBypassPressedEvent);
+bool BypassService::interestedIn(const Event& t_event) const {
+  if (t_event.m_subject == EventSubject::kSwitch
+      && t_event.m_action == EventAction::kPressed
+      && t_event.matchesId(SwitchId::kBypass)) return true;
+
+  if (t_event.m_subject == EventSubject::kSwitch
+      && t_event.m_action == EventAction::kValueChanged
+      && t_event.matchesId(SwitchId::kBypass)) return true;
+
+  return false;
 }
