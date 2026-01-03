5dce7d4eba9c3c069587da397b0ee485c9efe6ba
yann-ygn <yann@ygn-effects.com>
2026-01-03 21:49:40 +0100

    Initial rewrite of the MIDI service

--- firmware/fv1-platform-firmware/src/services/midi_service.h
+++ firmware/fv1-platform-firmware/src/services/midi_service.h
@@ -5,29 +5,34 @@
 #include "core/event_bus.h"
 #include "logic/logical_state.h"
 #include "logic/midi_handler.h"
+#include "ui/inputs.h"
 #include "utils/utils.h"
 
-constexpr EventType ccParamEventMap[] = {
-  EventType::kMidiPot0Moved,
-  EventType::kMidiPot1Moved,
-  EventType::kMidiPot2Moved,
-  EventType::kMidiMixPotMoved,
-  EventType::kMidiBypassPressed,
-  EventType::kMidiTapPressed,
-  EventType::kMidiTempoChanged,
-  EventType::kMidiProgramChanged,
-  EventType::kMidiProgramModeChanged
+struct MidiEventDefinition {
+  EventSubject m_subject;
+  EventAction m_action;
+  uint8_t m_id;
 };
+
+constexpr MidiEventDefinition c_ccMap[] = {
+  /* CC 0 */ { EventSubject::kPot, EventAction::kValueChanged, static_cast<uint8_t>(PotId::kPot0) },
+  /* CC 1 */ { EventSubject::kPot, EventAction::kValueChanged, static_cast<uint8_t>(PotId::kPot1) },
+  /* CC 2 */ { EventSubject::kPot, EventAction::kValueChanged, static_cast<uint8_t>(PotId::kPot2) },
+  /* CC 3 */ { EventSubject::kPot, EventAction::kValueChanged, static_cast<uint8_t>(PotId::kMixPot) },
+  /* CC 4 */ { EventSubject::kSwitch, EventAction::kValueChanged, static_cast<uint8_t>(SwitchId::kBypass) },
+  /* CC 5 */ { EventSubject::kSwitch, EventAction::kValueChanged, static_cast<uint8_t>(SwitchId::kTap) },
+  /* CC 6 */ { EventSubject::kTempo, EventAction::kValueChanged, 0 },
+  /* CC 7 */ { EventSubject::kProgram, EventAction::kValueChanged, 0 },
+  /* CC 8 */ { EventSubject::kProgram, EventAction::kDeltaChanged, 0 }
+};
+
+constexpr uint8_t c_ccMapSize = sizeof(c_ccMap) / sizeof(MidiEventDefinition);
 
 namespace MidiCCValues {
   constexpr uint8_t c_bypassDisable = 0;
   constexpr uint8_t c_bypassEnable = 127;
   constexpr uint8_t c_tapShortPress = 0;
   constexpr uint8_t c_tapLongPress = 127;
-}
-
-constexpr EventType ccParamToEvent(uint8_t t_ccParam) {
-  return ccParamEventMap[t_ccParam];
 }
 
 class MidiService : public Service {
@@ -45,7 +50,7 @@ class MidiService : public Service {
     void init() override;
     void handleEvent(const Event& t_event) override;
     void update() override;
-    bool interestedIn(EventCategory t_category, EventSubCategory t_subCategory) const override;
+    bool interestedIn(const Event& t_event) const override;
 
     // Tests
     MidiHandler* getMidiHandler() { return &m_midiHandler; }
