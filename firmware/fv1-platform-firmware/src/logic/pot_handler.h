e46855eaecccb70555dabc03d45f40fcda273d2c
yann-ygn <yann@ygn-effects.com>
2026-01-12 21:07:27 +0100

    Hackish but will work for now

--- firmware/fv1-platform-firmware/src/logic/pot_handler.h
+++ firmware/fv1-platform-firmware/src/logic/pot_handler.h
@@ -20,9 +20,9 @@ enum class PotState : uint8_t {
 using PotStateValidator = EnumUtils::EnumValidator<PotState, PotState::kDisabled, PotState::kActive>;
 
 struct PotHandler {
-  PotState m_state[PotConstants::c_potCount] = {PotState::kActive};
-  uint16_t m_minValue[PotConstants::c_potCount] = {0};
-  uint16_t m_maxValue[PotConstants::c_potCount] = {1023};
+  PotState m_state[PotConstants::c_potCount] = {PotState::kActive, PotState::kActive, PotState::kActive, PotState::kActive};
+  uint16_t m_minValue[PotConstants::c_potCount] = {0, 0, 0, 0};
+  uint16_t m_maxValue[PotConstants::c_potCount] = {1023, 1023, 1023, 1023};
 
   uint16_t mapMidiValue(uint8_t t_midiValue, uint8_t t_potIndex);
   uint16_t mapAdcValue(uint16_t t_adcValue, uint8_t t_potIndex);
