9df977da36871728aefb14f04c08115acc3cf3d1
yann-ygn <yann@ygn-effects.com>
2026-01-16 15:11:56 +0100

    Refactored FV1Service and tests with the new event system

--- firmware/fv1-platform-firmware/src/services/fv1_service.cpp
+++ firmware/fv1-platform-firmware/src/services/fv1_service.cpp
@@ -1,14 +1,6 @@
 #include "services/fv1_service.h"
 
-Fv1Service::Fv1Service(const LogicalState& t_lState, Fv1& t_fv1)
-  : m_logicalState(t_lState), m_fv1(t_fv1) {}
-
 void Fv1Service::init() {
-  for (uint8_t i = 0; i < PotConstants::c_potCount; i++) {
-    m_handler.m_potConfigs[i].m_minLogical = m_logicalState.m_potParams[m_logicalState.m_currentProgram][i].m_minValue;
-    m_handler.m_potConfigs[i].m_maxLogical = m_logicalState.m_potParams[m_logicalState.m_currentProgram][i].m_maxValue;
-  }
-
   m_fv1.sendProgramChange(m_logicalState.m_currentProgram);
 
   if (m_logicalState.m_activeProgram->m_isDelayEffect) {
@@ -25,36 +17,21 @@ void Fv1Service::init() {
 }
 
 void Fv1Service::handleEvent(const Event& t_event) {
-  switch(t_event.m_type) {
-    case EventType::kProgramChanged:
+  if (t_event.m_domain == EventDomain::kLogic) {
+    if (t_event.m_subject == EventSubject::kProgram
+        && t_event.m_action == EventAction::kValueChanged) {
       init();
-      break;
+    }
 
-    case EventType::kTempoChanged:
+    if (t_event.m_subject == EventSubject::kPot
+        && t_event.m_action == EventAction::kValueChanged) {
+      m_fv1.sendPotValue(static_cast<Fv1Pot>(t_event.m_id), m_logicalState.m_potParams[m_logicalState.m_currentProgram][t_event.m_id].m_value);
+    }
+
+    if (t_event.m_subject == EventSubject::kTempo
+        && t_event.m_action == EventAction::kValueChanged) {
       m_fv1.sendPotValue(Fv1Pot::Pot0, m_handler.mapTempoValue(m_logicalState.m_tempo));
-      break;
-
-    case EventType::kPot0Moved:
-    case EventType::kMenuPot0Moved:
-    case EventType::kMidiPot0Moved:
-      if (m_logicalState.m_activeProgram->m_isDelayEffect) { return; }
-      m_fv1.sendPotValue(Fv1Pot::Pot0, m_logicalState.m_potParams[0]->m_value);
-      break;
-
-    case EventType::kPot1Moved:
-    case EventType::kMenuPot1Moved:
-    case EventType::kMidiPot1Moved:
-      m_fv1.sendPotValue(Fv1Pot::Pot1, m_logicalState.m_potParams[1]->m_value);
-      break;
-
-    case EventType::kPot2Moved:
-    case EventType::kMenuPot2Moved:
-    case EventType::kMidiPot2Moved:
-      m_fv1.sendPotValue(Fv1Pot::Pot2, m_logicalState.m_potParams[2]->m_value);
-      break;
-
-    default:
-      break;
+    }
   }
 }
 
@@ -62,10 +39,17 @@ void Fv1Service::update() {
 
 }
 
-bool Fv1Service::interestedIn(EventCategory t_category, EventSubCategory t_subCategory) const {
-  return (t_category == EventCategory::kPhysicalEvent && t_subCategory == EventSubCategory::kPotEvent)
-      || (t_category == EventCategory::kTempoEvent && t_subCategory == EventSubCategory::kTempoChangedEvent)
-      || (t_category == EventCategory::kProgramEvent && t_subCategory == EventSubCategory::kProgramChangedEvent)
-      || (t_category == EventCategory::kMenuEvent && t_subCategory == EventSubCategory::kMenuPotEvent)
-      || (t_category == EventCategory::kMidiEvent && t_subCategory == EventSubCategory::kMidiPotMovedEvent);
+bool Fv1Service::interestedIn(const Event& t_event) const {
+  if (t_event.m_domain == EventDomain::kLogic) {
+    if (t_event.m_subject == EventSubject::kProgram
+        && t_event.m_action == EventAction::kValueChanged) return true;
+
+    if (t_event.m_subject == EventSubject::kTempo
+        && t_event.m_action == EventAction::kValueChanged) return true;
+
+    if (t_event.m_subject == EventSubject::kPot
+        && t_event.m_action == EventAction::kValueChanged) return true;
+  }
+
+  return false;
 }
